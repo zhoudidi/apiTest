6917b00bc08a6d4053b7e1628325064e279108af
zhoudidi <1035224696@qq.com>
2017-09-06 16:28:30 +0800

    redeit

---
+++ NovleGet/GetXiaohua/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+using System.IO;
+namespace GetXiaohua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb htmlWeb = new HtmlWeb();
+            HtmlDocument document = htmlWeb.Load("https://www.qiushibaike.com/");
+            HtmlNodeCollection nodeCollection = document.DocumentNode.SelectNodes(@"//div[@class='content']/span");
+
+             foreach (var node in nodeCollection)
+             {
+
+                 string values = node.InnerHtml;
+                 values = values.Replace("\n", "").Replace("<br>", "\r\n").Replace("查看全文","");
+                 FileStream fs = new FileStream("冷笑话.txt", FileMode.Append, FileAccess.Write);
+                 StreamWriter sr = new StreamWriter(fs, Encoding.UTF8);
+                 sr.WriteLine(values + "\r\n");// 开始写入
+                 sr.Close();
+                 fs.Close();
+             }
+        }
+    }
+}
---
+++ NovleGet/ImageGet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+namespace ImageGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb htmlWeb = new HtmlWeb();
+            HtmlDocument document = htmlWeb.Load("https://www.dbmeinv.com/");
+            HtmlNodeCollection nodeCollection = document.DocumentNode.SelectNodes(@"//div[@class='img_single']/a[@href]");
+               foreach (var node in nodeCollection)
+               {
+                   HtmlAttribute attribute = node.Attributes["href"];
+                   String val = attribute.Value;
+                   HtmlNodeCollection doc = htmlWeb.Load(val).DocumentNode.SelectNodes(@"//div[@class='topic-figure cc']/img");
+                   var u= doc[0].Attributes["src"].Value;
+                   //var title = htmlWeb.Load(val).DocumentNode.SelectNodes(@"//div[@class='bookread']/h1")[0].InnerText;  //文章标题
+               }
+        }
+    }
+}
